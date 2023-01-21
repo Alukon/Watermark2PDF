4080b644779d3b9fe104ad6b0c09d43e18fa0f69
Alukon <111476972+Alukon@users.noreply.github.com>
2023-01-21 12:58:35 +1000

    С мспользованием PdfFileReader, не работает

---
+++ main.py
@@ -0,0 +1,24 @@
+# pip install PyPDF2
+import PyPDF2
+
+input_file = 'textPDF.pdf'
+output_file = 'textPDF1.pdf'
+watermark_file = 'logo.pdf'
+
+with open(input_file, 'rb',) as file_input:
+    pdf = PyPDF2.PdfFileReader(file_input)
+
+    with open(watermark_file, 'rb') as file_watermark:
+        watermark = PyPDF2.PdfFileReader(file_watermark)
+
+        first_page = pdf.getPage(0)
+        first_page_watermark = watermark.getPage(0)
+
+        first_page.mergePage(first_page_watermark)
+
+        pdf_writer = PyPDF2.PdfFileWriter()
+
+        pdf_writer.addPage(first_page)
+
+        with open(output_file, 'wb') as file_output:
+            pdf_writer.write(file_output)
