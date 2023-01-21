0250c0f1e39c2e950a963154ed4c9554929d7b91
Alukon <111476972+Alukon@users.noreply.github.com>
2023-01-21 13:32:34 +1000

    С мспользованием Мартын-Назаров Кирилл, тоже не работает

--- main.py
+++ main.py
@@ -5,20 +5,29 @@ input_file = 'textPDF.pdf'
 output_file = 'textPDF1.pdf'
 watermark_file = 'logo.pdf'
 
-with open(input_file, 'rb',) as file_input:
-    pdf = PyPDF2.PdfReader(file_input)
+with open(input_file, "rb") as filehandle_input:
+    # читать содержимое исходного файла
+    pdf = PyPDF2.PdfFileReader(filehandle_input)
 
-    with open(watermark_file, 'rb') as file_watermark:
-        watermark = PyPDF2.PdfReader(file_watermark)
+    with open(watermark_file, "rb") as filehandle_watermark:
+        # читать содержание водяного знака
+        watermark = PyPDF2.PdfFileReader(filehandle_watermark)
 
-        first_page = pdf.pages[0]
-        first_page_watermark = watermark.pages[0]
+        # получить первую страницу оригинального PDF
+        first_page = pdf.getPage(0)
 
+        # получить первую страницу водяного знака PDF
+        first_page_watermark = watermark.getPage(0)
+
+        # объединить две страницы
         first_page.mergePage(first_page_watermark)
 
-        pdf_writer = PyPDF2.PdfWriter()
+        # создать объект записи PDF для выходного файла
+        pdf_writer = PyPDF2.PdfFileWriter()
 
+        # добавить страницу
         pdf_writer.addPage(first_page)
 
-        with open(output_file, 'wb') as file_output:
-            pdf_writer.write(file_output)
+        with open(output_file, "wb") as filehandle_output:
+            # записать файл с водяными знаками в новый файл
+            pdf_writer.write(filehandle_output)
