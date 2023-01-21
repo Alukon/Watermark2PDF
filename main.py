390a046b16cb90d7a10aa8f743f5ca469ac8bb26
Alukon <111476972+Alukon@users.noreply.github.com>
2023-01-21 13:09:49 +1000

    С мспользованием PdfReader, тоже не работает

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ output_file = 'textPDF1.pdf'
 watermark_file = 'logo.pdf'
 
 with open(input_file, 'rb',) as file_input:
-    pdf = PyPDF2.PdfFileReader(file_input)
+    pdf = PyPDF2.PdfReader(file_input)
 
     with open(watermark_file, 'rb') as file_watermark:
-        watermark = PyPDF2.PdfFileReader(file_watermark)
+        watermark = PyPDF2.PdfReader(file_watermark)
 
         first_page = pdf.getPage(0)
         first_page_watermark = watermark.getPage(0)
 
         first_page.mergePage(first_page_watermark)
 
-        pdf_writer = PyPDF2.PdfFileWriter()
+        pdf_writer = PyPDF2.PdfWriter()
 
         pdf_writer.addPage(first_page)
 
